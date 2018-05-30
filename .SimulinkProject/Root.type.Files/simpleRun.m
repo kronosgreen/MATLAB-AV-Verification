4a3cf58a121ca18226cc074bc1a22e60dacc0379
Chris Medrano <cmedranoberumen2844@floridapoly.edu>
2018-05-30 11:02:20 -0400

    Reverse paths & forward paths are all fixed

--- .SimulinkProject/Root.type.Files/simpleRun.m
+++ .SimulinkProject/Root.type.Files/simpleRun.m
@@ -7,9 +7,9 @@ clc
 % Num of simulations to run in parallel
 NUM_SIMULATIONS = 5;
 % Num of road pieces to place in scenario
-LEN_ROAD = 8;
+LEN_ROAD = 10;
 % Num of actors to place in scenario (cars, pedestrians, etc.)
-NUM_ACTORS = 10;
+NUM_ACTORS = 14;
 
 runSimulations(NUM_SIMULATIONS, LEN_ROAD, NUM_ACTORS);
 
